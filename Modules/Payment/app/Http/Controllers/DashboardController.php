858ba534ea2221f84f26723c15f21bdbc4a2855c
unknown <kamrankhan3.61k@gmail.com>
2025-10-27 23:03:40 +0500

    modified the  dashboardController

--- Modules/Payment/app/Http/Controllers/DashboardController.php
+++ Modules/Payment/app/Http/Controllers/DashboardController.php
@@ -2,55 +2,44 @@
 
 namespace Modules\Payment\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Modules\Payment\Entities\PaymentTransaction;
+use Modules\Payment\Transformers\PaymentResource;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        return view('payment::index');
+        $transactions = PaymentTransaction::with(['user', 'order'])
+            ->latest()
+            ->paginate(20);
+
+        if ($request->wantsJson()) {
+            return PaymentResource::collection($transactions);
+        }
+
+        return view('payment::dashboard.index', compact('transactions'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function show(PaymentTransaction $transaction)
     {
-        return view('payment::create');
+        return view('payment::dashboard.show', compact('transaction'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request) {}
-
-    /**
-     * Show the specified resource.
-     */
-    public function show($id)
+    public function statistics()
     {
-        return view('payment::show');
+        $totalTransactions = PaymentTransaction::count();
+        $totalRevenue = PaymentTransaction::completed()->sum('amount');
+        $successfulTransactions = PaymentTransaction::completed()->count();
+        $pendingTransactions = PaymentTransaction::pending()->count();
+
+        return response()->json([
+            'total_transactions' => $totalTransactions,
+            'total_revenue' => $totalRevenue,
+            'successful_transactions' => $successfulTransactions,
+            'pending_transactions' => $pendingTransactions,
+            'success_rate' => $totalTransactions > 0 ? ($successfulTransactions / $totalTransactions) * 100 : 0
+        ]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('payment::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id) {}
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id) {}
 }
